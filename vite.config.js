6631f427e6da4878bc4faac8251c8312157ec72d
OKPN <narrative.verifier@proton.me>
2026-08-05 14:08:12 +0900

    fix: update Vite base path to relative ./ to support new byoc-converter repository name

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/byoc-publisher/" : "/",
+  base: "./",
   worker: {
     format: "es",
   },
